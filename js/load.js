1297bae037d870ec7bd78ff4e74a81ce84f8fdea
Tom Poyser <thomas.poyser@student.manchester.ac.uk>
2025-03-03 16:35:22 +0000

    load medical diagnosis visualisation on first load

--- js/load.js
+++ js/load.js
@@ -1,15 +1,47 @@
-window.onload = function(){
+window.onload = function () {
+    loadPresets();
+
     const savedState = localStorage.getItem('current-state');
-    if (savedState) {
+    let stateToLoad = savedState;
+
+    // for first time opening site:
+    // if 'current-state' doesn't exist, use 'saved_state_Medical_Diagnosis'
+    if (!savedState) {
+        stateToLoad = localStorage.getItem('saved_state_Medical_Diagnosis');
+    }
+
+    if (stateToLoad) {
         try {
-            const parsedState = JSON.parse(savedState);
+            const parsedState = JSON.parse(stateToLoad);
             loadState(parsedState);
         } catch (error) {
-            console.error('Error parsing saved state:', error);
+            console.error('Error parsing state:', error);
         }
     }
-    // drawTree();
-    // toggleKey();
 
-    setTimeout(() => infoPopup('<b>Left-click + Drag</b> to Pan<br><b>Scroll</b> to Zoom',5), 1000);
+    setTimeout(() => {
+        infoPopup('<b>Left-click + Drag</b> to Pan<br><b>Scroll</b> to Zoom', 5);
+    }, 2000);
 };
+
+
+function loadPresets() {
+    const medicalDiagnosisPreset = 'saved_state_Medical_Diagnosis';
+
+    if (!localStorage.getItem(medicalDiagnosisPreset)) {
+        fetch('data/medical_diagnosis_visualisation.json')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Failed to fetch the JSON file');
+                }
+                return response.json();
+            })
+            .then(data => {
+                localStorage.setItem(medicalDiagnosisPreset, JSON.stringify(data));
+                console.log('Preset data loaded into localStorage.');
+            })
+            .catch(error => console.error('Error loading presets:', error));
+    } else {
+        console.log('Presets already exist in localStorage.');
+    }
+}

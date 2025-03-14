46fb20962203629eedc6490463d4a2c0f446c365
Tom Poyser <thomas.poyser@student.manchester.ac.uk>
2025-03-14 17:58:11 +0000

    info added

--- js/home.js
+++ js/home.js
@@ -5,3 +5,12 @@ document.getElementById("home-start-btn").addEventListener("click", function() {
   document.getElementById("home-tutorial-btn").addEventListener("click", function() {
     window.location.href = "visualisation.html?tutorial=true";
   });
+
+
+function openInfo(){
+  document.getElementById('home-info-background').style.display = 'flex';
+}
+
+function closeInfo(){
+  document.getElementById('home-info-background').style.display = 'none';
+}
